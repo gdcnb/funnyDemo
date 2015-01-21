2ffadc0b27db67065919ceee8747838e156eeaa7
gdcnb <4013531@qq.com>
2015-01-21 14:25:30 +0800

    [ADD] 添加页面滚动动画

--- components/scrollAnimate/scrollAnimate.js
+++ components/scrollAnimate/scrollAnimate.js
@@ -3,12 +3,19 @@
  * @param domId css选择器
  */
 function scrollAnimate(domId) {
-    var targetDom = document.querySelector(domId),
-        viewH = window.innerHeight,
-        pageH = document.body.offsetHeight,
-        scrollH = document.body.scrollHeight;
+    var targetDom = document.querySelector(domId);
 
-    clearInterval(targetDom.timer);
+    targetDom.timer = setInterval(function(){
+        var scrollDistance = getScrollDistance(targetDom),
+            speed = scrollDistance > 0 ? Math.ceil(scrollDistance/5) : Math.floor(scrollDistance/5);
+
+        if(scrollDistance === 0){
+            clearInterval(targetDom.timer);
+        } else {
+            document.body.scrollTop += speed;
+        }
+    }, 20);
+
     // private function
     /**
      * 获取滚动条需要滚动的距离，正数为向下滚动，负数为向上滚动
@@ -18,7 +25,11 @@ function scrollAnimate(domId) {
     function getScrollDistance(elem) {
         var distance = elem.offsetTop, //元素外边框距离父元素内边框距离
             parent = elem.offsetParent,
-            scrollY = document.body.scrollTop;
+            scrollY = document.body.scrollTop,
+            viewH = window.innerHeight,
+            pageH = document.body.offsetHeight,
+            scrollH = document.body.scrollHeight,
+            resultH = 0;
 
         while(parent){
             distance += parent.clientTop; //加上父元素边框宽度
@@ -26,33 +37,35 @@ function scrollAnimate(domId) {
             parent = parent.offsetParent;
         }
 
-        return distance - scrollY;
-    }
+        /*var logDom = document.getElementById('showLog');
+        logDom.innerHTML = 'viewH = '+ viewH +'<br>' +
+            'distance = '+ (distance) +'<br>' +
+            'distance - scrollY = '+ (distance - scrollY) +'<br>' +
+            'scrollH = '+ scrollH +'<br>' +
+            'scrollY = '+ scrollY +'<br>' +
+            'scrollH - viewH = '+ (scrollH - viewH) +'<br>' +
+            'viewH = '+ viewH;
+*/
+        //TODO 判断当前滚动条的位置
+        var maxDist = scrollH - viewH;
+        if(pageH < viewH) {
+            //页面内容高度不满一屏时不用滚动
+            resultH = 0;
+        } else if (distance >= maxDist && scrollY >= maxDist) {
+            //当滚动的元素在最后一屏的范围内时，只需要滚动到最后一屏的高度
+            resultH = 0;
+        } else {
+            resultH = distance - scrollY;
+        }
 
-    //只有页面高度比
-    if((pageH > viewH) && targetDom) {
-        targetDom.timer = setInterval(function(){
-            var scrollDistance = getScrollDistance(targetDom),
-                speed = scrollDistance > 0 ? Math.ceil(scrollDistance/5) : Math.floor(scrollDistance/5);
-
-            //窗口高度会受到手机地址栏的影响，所以每次执行都重新拿一下值
-            viewH = window.innerHeight;
-
-            if(scrollDistance === 0){
-                clearInterval(targetDom.timer);
-            } else {
-                document.body.scrollTop += speed;
-
-                //TODO 手机上的地址栏会影响viewH的高度从而引发bug
-                var checkH = document.body.scrollTop + viewH;
-                if(checkH >= scrollH) { //当需要滚动的模块在最后一屏时结束运动
-                    alert('scrollDistance=' + scrollDistance + '; viewH=' + viewH + 'checkH=' + checkH + '; pageH=' + pageH);
-                    clearInterval(targetDom.timer);
-                }
-            }
-        }, 20);
+        return resultH
     }
 }
+
+//启动scroll事件更新viewH，解决地址栏引发的视图高度变化的问题
+window.addEventListener('scroll', function(){
+    var viewH = window.innerHeight;
+});
 
 window.scrollAnimate = scrollAnimate;
 
