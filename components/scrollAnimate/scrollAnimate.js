d092835be97084b82c4762da711fc2f6059a9538
chennanbei <4013531@qq.com>
2015-03-30 00:19:30 +0800

    [upd] 调整获取元素相对页面高度的位置

--- components/scrollAnimate/scrollAnimate.js
+++ components/scrollAnimate/scrollAnimate.js
@@ -1,3 +1,41 @@
+
+/* private function */
+function clearIntervalTimer() {
+    //当在滚动过程中有触发 touchmove 事件时则停止动画
+    clearInterval(targetDom.timer);
+}
+
+function scrollEvent() {
+    var viewH = window.innerHeight;
+}
+/**
+ * 获取滚动条需要滚动的距离，正数为向下滚动，负数为向上滚动
+ * @param elem
+ * @returns {number}
+ */
+function getScrollDistance(elem) {
+    var scrollY = window.pageYOffset,
+        distance = elem.getBoundingClientRect().top + scrollY, //元素外边框距离父元素内边框距离
+        viewH = window.innerHeight,
+        pageH = document.body.offsetHeight,
+        scrollH = document.body.scrollHeight,
+        resultH = 0;
+
+    //判断当前滚动条的位置
+    var maxDist = scrollH - viewH;
+    if(pageH < viewH) {
+        //页面内容高度不满一屏时不用滚动
+        resultH = 0;
+    } else if (distance >= maxDist && scrollY >= maxDist) {
+        //当滚动的元素在最后一屏的范围内时，只需要滚动到最后一屏的高度
+        resultH = 0;
+    } else {
+        resultH = distance - scrollY;
+    }
+
+    return resultH
+}
+
 /**
  * 页面滚动动画插件，提供一个页面元素的id号,之后它会滚动到该元素的位置
  * @param domId css选择器
@@ -27,50 +65,6 @@ function scrollAnimate(domId, callback) {
     window.addEventListener('scroll', scrollEvent, false);
 
     document.body.addEventListener('touchmove', clearIntervalTimer, false);
-
-    /* private function */
-    function clearIntervalTimer() {
-        //当在滚动过程中有触发 touchmove 事件时则停止动画
-        clearInterval(targetDom.timer);
-    }
-
-    function scrollEvent() {
-        var viewH = window.innerHeight;
-    }
-    /**
-     * 获取滚动条需要滚动的距离，正数为向下滚动，负数为向上滚动
-     * @param elem
-     * @returns {number}
-     */
-    function getScrollDistance(elem) {
-        var distance = elem.offsetTop, //元素外边框距离父元素内边框距离
-            parent = elem.offsetParent,
-            scrollY = document.body.scrollTop,
-            viewH = window.innerHeight,
-            pageH = document.body.offsetHeight,
-            scrollH = document.body.scrollHeight,
-            resultH = 0;
-
-        while(parent){
-            distance += parent.clientTop; //加上父元素边框宽度
-            distance += parent.offsetTop; //加上父元素外边框到下个包含元素内边框距离
-            parent = parent.offsetParent;
-        }
-
-        //判断当前滚动条的位置
-        var maxDist = scrollH - viewH;
-        if(pageH < viewH) {
-            //页面内容高度不满一屏时不用滚动
-            resultH = 0;
-        } else if (distance >= maxDist && scrollY >= maxDist) {
-            //当滚动的元素在最后一屏的范围内时，只需要滚动到最后一屏的高度
-            resultH = 0;
-        } else {
-            resultH = distance - scrollY;
-        }
-
-        return resultH
-    }
 }
 
 window.scrollAnimate = scrollAnimate;
